7b989922d04bd3259a18db87cc239c998586986e
QuantumSheep <quantumsheep@outlook.fr>
2018-09-15 13:40:55 +0200

    Change prefix manager

--- public/assets/js/gate.js
+++ public/assets/js/gate.js
@@ -1,6 +1,8 @@
 const updatePrefix = () => {
-    document.getElementById('cmd-prefix').innerText = `root@${username} >`;
+    document.getElementById('cmd-prefix').innerText = `${user.username}@gate${user._id} >`;
 }
+
+updatePrefix();
 
 window.addEventListener('click', function(e) {
     document.getElementById('command').focus();
