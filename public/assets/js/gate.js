cbc59397fdaad64e4be2e61e24c4d42557c90e37
QuantumSheep <nathanael.dmc@outlook.fr>
2018-09-16 15:00:07 +0200

    Add input system

--- public/assets/js/gate.js
+++ public/assets/js/gate.js
@@ -1,7 +1,24 @@
-const updatePrefix = () => {
-    document.getElementById('cmd-prefix').innerText = `root@${username} >`;
+const terminal = {
+    prefix: document.getElementById('cmd-prefix'),
+    content: document.getElementById('cmd-content'),
+    input: document.getElementById('cmd-input'),
 }
 
-window.addEventListener('click', function(e) {
-    document.getElementById('command').focus();
+const updatePrefix = () => {
+    terminal.prefix.innerText = `root@${username} >`;
+}
+
+window.addEventListener('click', function (e) {
+    terminal.input.focus();
+});
+
+terminal.input.addEventListener('input', function (e) {
+    terminal.content.innerText = this.value;
+});
+
+document.getElementById('terminal').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    terminal.input.value = '';
+    terminal.content.innerText = '';
 });
