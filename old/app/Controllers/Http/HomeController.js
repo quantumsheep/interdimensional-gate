95594127b9f3806133f178124d777a8996806961
QuantumSheep <nathanael.dmc@outlook.fr>
2018-09-20 23:01:26 +0200

    Remove Adonis directory

--- old/app/Controllers/Http/HomeController.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-class HomeController {
-    async index({ view, auth }) {
-        return view.render('index');
-    }
-}
-
-module.exports = HomeController;
--- old/start/socket.js
+++
@@ -1,4 +0,0 @@
-/** @type {import('@adonisjs/websocket/src/Ws/index')} */
-const Ws = use('Ws')
-
-Ws.channel('os', 'OSController')
