abd8e40cde9c186bee80c9a43b5a2308405ff24b
QuantumSheep <quantumsheep@outlook.fr>
2018-10-11 18:27:09 +0200

    Add challenge base code

--- server/src/commands/challenge.js
+++ server/src/commands/challenge.js
@@ -2,6 +2,31 @@
  * @param {import('../GateOS')} os 
  * @param {string[]} args
  */
-exports.call = (os, args) => {
-    os.row(['instructions.md']).end();
+exports.call = (os, [action]) => {
+    if (!action) {
+        os.help();
+    }
+
+    if (action === 'create') {
+        os.setState();
+        os.input('Challenge name:');
+    }
+}
+
+/**
+ * @param {import('../GateOS')} os 
+ * @param {string[]} args
+ */
+exports.state = (os, value) => {
+    if(!os.state.data.name) {
+        os.setState({ name: value });
+        os.input('Challenge description:', 'textarea');
+        return;
+    }
+
+    if(!os.state.data.description) {
+        os.setState({ description: value });
+        os.input('');
+        return;
+    }
 }
--- server/src/models/challenge.js
+++ server/src/models/challenge.js
@@ -10,6 +10,7 @@ const schema = new db.Schema({
     type: { type: String, required: true },
     difficulty: { type: Number, required: true, default: -1 },
     description: { type: String, required: true },
+    inputs: { type: [[String]], required: true },
     answers: { type: [Answers], required: true, default: [] },
 });
 
